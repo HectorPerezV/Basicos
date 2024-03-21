e2d2436b74e5972b1312fae5fe36f19d6046dd7e
Hpzv26 <159396342+Hpzv26@users.noreply.github.com>
2024-03-20 20:26:41 -0600

    Update jsBasicos.js

--- jsBasicos.js
+++ jsBasicos.js
@@ -3,5 +3,18 @@ const log=document.getElementById("log");
 
 input.addEventListener("keydown", logKey);
 function logKey(e){
-    log.textContent += `${e.code}`;
+    if(e.code=="ArrowUp"){
+        console.log("esta brincando")
+    }
+    if(e.code=="ArrowDown"){
+        console.log("esta agachado")
+    }
+    if(e.code=="ArrowRight"){
+        console.log("esta avanzando")
+    }
+    if(e.code=="ArrowLeft"){
+        console.log("esta retrocediendo")
+    }
+
+   // log.textContent += `${e.code}`;
 }
